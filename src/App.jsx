0c868827f5d51278557ead47969ab9d100de6f42
devsamuelsilva <devsamuelsilva@gmail.com>
2024-10-10 21:21:13 -0300

    Istalando o bootstrap e criando meu primeiro formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="form-pessoa">
+        <h2>Cadastro de Pessoas</h2>
+        <form className="row g-3">
+        <div className="col-md-6">
+            <label htmlFor="nomeCompleto" className="form-label">
+              Nome Completo
+            </label>
+            <input
+              type="texte"
+              className="form-control"
+              id="nome"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="nomeDaMae" className="form-label">
+              Nome da Mãe
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="nomeDaMae"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Digite o Email"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="senha" className="form-label">
+              Senha
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Digite a Senha"
+            />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="cep" className="form-label">
+              Cep
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="password"
+              placeholder="Digite a cep"
+            />
+          </div>
+          <div className="col-md-8">
+            <label htmlFor="endereco" className="form-label">
+              Endereço
+            </label>
+            <input type="text" className="form-control" id="endereco" />
+          </div>
+          <div className="col-md-1">
+            <label htmlFor="numero" className="form-label">
+              Numero
+            </label>
+            <input type="number" className="form-control" id="numero" />
+          </div>
+          <div className="col-md-11">
+            <label htmlFor="complemento" className="form-label">
+              Complemento
+            </label>
+            <input type="text" className="form-control" id="complemento" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="bairro" className="form-label">
+              Bairro
+            </label>
+            <input type="text" className="form-control" id="bairro" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="estado" className="form-label">
+              Estado
+            </label>
+            <select className="form-select" id="estado">
+              <option selected> Escolha...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="cidade" className="form-label">
+              Cidade
+            </label>
+            <select className="form-select" id="cidade">
+              <option selected> Escolha...</option>
+              <option>...</option>
+            </select>
+          </div>
+        </form>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
